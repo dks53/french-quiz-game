232d3f430fb46c8ccba6c53d4f83be96dd390df6
Darshil Shah <dks53@georgetown.edu>
2020-04-30 11:11:32 +0530

    Playing around with HTML routes and templates

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,21 +1,22 @@
 # web_app/routes/home_routes.py
 
-from flask import Blueprint #, render_template
+from flask import Blueprint, render_template
 
 home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
 def index():
     print("VISITED THE HOME PAGE")
-    #return render_template("dashboard.html")
-    return "Welcome Home (TODO)"
+    #return "Welcome Home (TODO)"
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("VISITED THE ABOUT PAGE")
-    return "About Me (TODO)"
+    #return "About Me (TODO)"
+    return render_template("about.html")
 
-@home_routes.route("/register")
+@home_routes.route("/user/new")
 def register():
     print("VISITED THE REGISTRATION PAGE")
-    return "Sign Up for our Product! (TODO)"
+    return "New user page"
