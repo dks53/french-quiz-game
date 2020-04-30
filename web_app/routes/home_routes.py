ab6e75053da10d8a8fdf783632d22897dc03ac9b
Darshil Shah <dks53@georgetown.edu>
2020-04-30 12:22:03 +0530

    Navigating from one HTML page to another. Runs locally

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -16,7 +16,20 @@ def about():
     #return "About Me (TODO)"
     return render_template("about.html")
 
+@home_routes.route("/instructions")
+def rules():
+    print("VISITED THE INSTRUCTIONS PAGE")
+    #return "How to play the game (TODO)"
+    return render_template("instructions.html")
+
 @home_routes.route("/user/new")
 def register():
     print("VISITED THE REGISTRATION PAGE")
-    return "New user page"
+    #return "New user page (TODO)"
+    return render_template("signup.html")
+
+@home_routes.route("/user/create")
+def regisetered():
+    print("VISITED USER CREATED PAGE")
+    #return "New user created page (TODO)"
+    return render_template("created_user.html")
